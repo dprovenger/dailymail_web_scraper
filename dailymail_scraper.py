9785b287433fba5907451762bbfeda09331ee4b8
Jorge Arrieta <jorge.r.arrieta.jr@gmail.com>
2020-12-19 10:24:51 -0600

    Pulling articles headline

--- dailymail_scraper.py
+++ dailymail_scraper.py
@@ -5,4 +5,7 @@ source = requests.get('https://www.dailymail.co.uk/ushome/index.html').text
 soup = BeautifulSoup(source, 'lxml')
 
 article = soup.find('div', class_='article-tri-headline')
-print(article.prettify())
+#print(article.prettify())
+
+headline = article.h2.a.text 
+print(headline)
