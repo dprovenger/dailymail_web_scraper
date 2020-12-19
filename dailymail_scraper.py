e47aeaacbb15622d3e85d11d3cab4713afbf8af3
Jorge Arrieta <jorge.r.arrieta.jr@gmail.com>
2020-12-19 10:31:03 -0600

    Adding a loop to get all articles from site

--- dailymail_scraper.py
+++ dailymail_scraper.py
@@ -4,16 +4,17 @@ import requests
 source = requests.get('https://www.dailymail.co.uk/ushome/index.html').text 
 soup = BeautifulSoup(source, 'lxml')
 
-article = soup.find('div', class_='article-tri-headline')
-#print(article.prettify())
+for article in soup.find_all('div', class_='article-tri-headline'):
+    #print(article.prettify())
 
-headline = article.h2.a.text 
-print(headline)
+    headline = article.h2.a.text 
+    print(headline)
 
-summary = article.p.text
-print(summary)
+    summary = article.p.text
+    print(summary)
 
-video_source = soup.find('button')['data-url']
-video_url = video_source.split('?u=')[1]
-video_url_true = video_url.split('%3')[0]
-print(video_url_true)
+    video_source = soup.find('button')['data-url']
+    video_url = video_source.split('?u=')[1]
+    video_url_true = video_url.split('%3')[0]
+    print(video_url_true)
+    print()
