a313d95e7d5e1e7773302438b447943fef569536
Jorge Arrieta <jorge.r.arrieta.jr@gmail.com>
2020-12-19 10:39:01 -0600

    Formatting output

--- dailymail_scraper.py
+++ dailymail_scraper.py
@@ -8,13 +8,20 @@ for article in soup.find_all('div', class_='article-tri-headline'):
     #print(article.prettify())
 
     headline = article.h2.a.text 
+    print()
+    print('--- HEADLINE: ---')
     print(headline)
+    print()
 
     summary = article.p.text
+    print('--- SUMMARY: ---')
     print(summary)
+    print()
 
     video_source = soup.find('button')['data-url']
     video_url = video_source.split('?u=')[1]
     video_url_true = video_url.split('%3')[0]
+    print('--- LINK TO FULL ARTICLE: ---')
     print(video_url_true)
     print()
+    print('=======================')
