093613c638b7935f957d149e2f1c2a126d46e704
Jorge Arrieta <jorge.r.arrieta.jr@gmail.com>
2020-12-19 10:21:22 -0600

    Requesting initial info for 1st article

---
+++ dailymail_scraper.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+import requests
+
+source = requests.get('https://www.dailymail.co.uk/ushome/index.html').text 
+soup = BeautifulSoup(source, 'lxml')
+
+article = soup.find('div', class_='article-tri-headline')
+print(article.prettify())
